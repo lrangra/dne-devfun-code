fc3459c4310dc4bcf5448da12ec92e0c089ab88e
lrangra <82490651+lrangra@users.noreply.github.com>
2024-01-30 20:00:26 +0530

    Update studentofdeckofcards.py

--- rest-api/python/studentofdeckofcards.py
+++ rest-api/python/studentofdeckofcards.py
@@ -1,10 +1,17 @@
 import requests
 
-url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
+url = "https://deckofcardsapi.com/api/deck/new/shuffle/"
+
+querystring = {"deck_count": "6"}
 
 payload = {}
-headers = {}
+headers = {
+   'Cache-Control': "no-cache",
+   'Postman-Token': "dd1d8ca5-7000-21b2-2230-39969d585419"}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url, headers=headers, params=querystring, data=payload)
 
+deck = respnose.json()
+deck_id = deck['deck_id']
+print(deck_id)
 print(response.text)
